59ed4e92f9bfb54fec7106fa6fdb286522c75272
Sertaniya <122295276+Sertaniya@users.noreply.github.com>
2023-01-10 21:26:40 +0200

    the second commint

---
+++ lesson_02/lesson02_Demydenko_Anastasiia.py
@@ -0,0 +1,17 @@
+# input()
+print("enter your first name: ")
+first_name_ = input()
+print("enter your surname: ")
+surname_ = input()
+print("enter your surname: ")
+
+# concatenation
+first_name = "Anastasiia"
+print(first_name)
+Surname = "Demydenko"
+Surname = first_name + ' ' + surname_
+print(full_name)
+
+#string
+double_name = name*2
+print(double_name)
