cc815afaeae8389d760ce37b829fe9b3224decf9
Sertaniya <122295276+Sertaniya@users.noreply.github.com>
2023-01-11 22:46:19 +0200

    Commint 10 jan 2023 task 02

--- lesson_02/lesson02_Demydenko_Anastasiia.py
+++ lesson_02/lesson02_Demydenko_Anastasiia.py
@@ -1,17 +1,51 @@
+import math
+
+1/1
 # input()
 print("enter your first name: ")
 first_name_ = input()
-print("enter your surname: ")
+print("enter your Surname: ")
 surname_ = input()
-print("enter your surname: ")
+print("enter your Surname: ")
 
 # concatenation
-first_name = "Anastasiia"
-print(first_name)
-Surname = "Demydenko"
-Full_name = first_name + ' ' + surname_
-print(full_name)
+first_name_ = "Anastasiia"
+print(first_name_)
+surname_ = "Demydenko"
+Full_name = first_name_ + surname_
+print(Full_name)
+
+1.2
+first_name = "anastasiia"
+surname_ = "demydenko"
+print(first_name_.title())
+print(surname_.title())
+print(surname_.upper())
+print((surname_.lower()))
+
+1.3
 
 #string
-double_name = first_name*2
+double_name = first_name*6
 print(double_name)
+
+
+1.4
+first_name = "Anastasiia"
+surname_ = "Demydenko"
+print("\tAnastasiia")
+print("\nDemydenko")
+
+Knowledge_is="power "
+print(Knowledge_is.rstrip())
+
+2.
+import math
+print("enter radius")
+r=float(input())
+s=math.pi*(r**2)
+l=2*math.pi*r
+print(f'square {s}')
+print(f'langue {l}')
+
+
